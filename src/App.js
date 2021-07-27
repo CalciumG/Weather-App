b1bc83839b8cb026860ed99eb13c3344ea128374
Callum <callumgo@outlook.com>
2021-07-27 22:55:27 +0100

    Removed unused code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.css";
 import ComponentWithGeolocation from "./Components/ComponentWithGeolocation";
-
-const api = {
-  key: "01e996920f65e535c780c5ad7bef78ce",
-  base: "http://api.openweathermap.org/data/2.5/",
-};
-
-const cityApi = {
-  key: "b6b6fa899ed2921a32d25deb7e6db975",
-  base: "http://api.positionstack.com/v1/reverse?",
-};
 
 function App() {
   return (
