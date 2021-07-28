046182318dcaed0f16a72538f15b7418b3d46009
Callum <callumgo@outlook.com>
2021-07-28 22:06:46 +0100

    try https

--- src/Components/ComponentWithGeolocation.js
+++ src/Components/ComponentWithGeolocation.js
@@ -3,12 +3,12 @@ import useGeolocation from "react-hook-geolocation";
 
 const api = {
   key: "01e996920f65e535c780c5ad7bef78ce",
-  base: "http://api.openweathermap.org/data/2.5/",
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
 const cityApi = {
   key: "b6b6fa899ed2921a32d25deb7e6db975",
-  base: "http://api.positionstack.com/v1/reverse?",
+  base: "https://api.positionstack.com/v1/reverse?",
 };
 
 function ComponentWithGeolocation() {
